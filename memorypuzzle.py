ac49a0cfd2ea6e4811af442526c761a49ac5f073
Kenneth <canowa@libero.it>
2021-12-16 09:37:24 +0100

    added first basic draft of main program

--- memorypuzzle.py
+++ memorypuzzle.py
@@ -1,7 +1,8 @@
 # MEMORY PUZZLE: main program file
 
 
-import pygame
+import pygame, sys
+from src import constants as CST
 from src import board
 from src import card
 
@@ -12,7 +13,45 @@ pygame.init()
 
 
 def main_program():
-    print("Executed main program!")
+
+
+    mainscreen = CST.MAINSCREEN
+    gameclock = pygame.time.Clock()
+    looping = True
+
+    a_color = CST.CARDCOLORS[0]
+
+    cardlist = [
+        card.Card((200,200), 64, CST.SHAPELIST[4], a_color, CST.CARD_BACK),
+        card.Card((64, 64), 128, CST.SHAPELIST[4], a_color, CST.CARD_BACK),
+    ]
+
+
+    while looping:
+        gameclock.tick(CST.FPS)
+        mousepos = None
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                looping = False
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                mousepos = pygame.mouse.get_pos()
+                for singlecard in cardlist:
+                    if singlecard.mouseover(mousepos):
+                        singlecard.card_flip()
+                        break        
+
+        mainscreen.fill((125,125,125))
+
+        for singlecard in cardlist:
+            singlecard.game_tick_update(mainscreen, mousepos)
+        
+        pygame.display.update()
+
+
+    pygame.quit()
+    sys.exit()
 
 
 
