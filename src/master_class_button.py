9fc8908d16b26bf3b473e3d4c29cb5a54076c692
Kenneth <canowa@libero.it>
2021-12-23 09:57:54 +0100

    Button text work properly

--- src/master_class_button.py
+++ src/master_class_button.py
@@ -25,8 +25,19 @@ class Button:
         self.button_border_radius = min(self.button_rect.width, self.button_rect.height) // 4
         self.func_to_call = func_to_call
         self.func_parameters = func_parameters
-        self.font_surf = pygame.font.Font(self.style["button_font"], 18)
 
+        # Text Surface generation (we adapt its size to button size)
+        margin = self.button_rect.height // 5
+        size = 1 # starting size
+        text_dim = (0,0)
+        max_width = self.button_rect.width - margin
+        max_height = self.button_rect.height - margin
+
+        while text_dim[0] < max_width and text_dim[1] < max_height:
+            text_dim = pygame.font.Font(self.style["button_font"], size).size(self.label)
+            size += 1
+
+        self.font_surf = pygame.font.Font(self.style["button_font"], size-1)
 
 
     def _check_if_mouse_over(self, mousepos: tuple) -> bool:
@@ -56,8 +67,62 @@ class Button:
         # Blitting on screen
         surf_to_blit = pygame.Surface((self.button_rect.width, self.button_rect.height))
         surf_to_blit.set_colorkey((0,0,0))
-
-        # TODO: add font render
+        surf_to_blit = self.font_surf.render(self.label, True, (0,255,0))
+        
         pygame.draw.rect(window, actual_color, self.button_rect, border_radius=self.button_border_radius)
-        surf_to_blit = self.font_surf.render(self.label, False, (0,255,0))
-        window.blit(surf_to_blit, (self.button_rect.x, self.button_rect.y))
+        text_pos = (self.button_rect.centerx - surf_to_blit.get_width()//2, self.button_rect.centery - surf_to_blit.get_height()//2)
+        window.blit(surf_to_blit, text_pos)
+
+
+
+
+
+
+
+
+
+# TESTING
+if __name__ == "__main__":
+    import constants as CST
+    import sys
+
+    pygame.init()
+
+
+    # dummy method
+    def tell(what: str):
+        print("I'm telling:", what)
+
+    mainscreen = CST.MAINSCREEN
+    gameclock = pygame.time.Clock()
+    looping = True
+
+    updatelist = [
+        Button("Test1", CST.BUTTON_STYLE, (50,50, 150, 50), tell, "test1"),
+        Button("Test2", CST.BUTTON_STYLE, (150,200, 150, 50), tell, "test2"),
+        Button("This is a long text for a button", CST.BUTTON_STYLE, (300,300, 150, 50), tell, "looong text"),
+        Button("Test4", CST.BUTTON_STYLE, (450,350, 150, 50), tell, "test 4"),
+    ]
+
+    while looping:
+        delta = gameclock.tick(CST.FPS)
+        mousepos = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                looping = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                for element in updatelist:
+                    if isinstance(element, Button):
+                        element.mouse_clicked(mousepos)
+
+        
+
+        mainscreen.fill((125,125,125))
+        for element in updatelist:
+            element.game_tick_update(mainscreen, mousepos, delta)
+        pygame.display.update()
+
+
+    pygame.quit()
+    sys.exit()
