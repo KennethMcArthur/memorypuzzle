db31f5f2e7b01816159a159d7f63d5b1c8ccd06e
Kenneth <canowa@libero.it>
2022-01-04 09:57:46 +0100

    started working on main level and board

--- src/board.py
+++ src/board.py
@@ -1,4 +1,108 @@
-# BOARD ELEMENT
+# MEMORY PUZZLE: Board element
+
+
+import pygame
+import constants as CST
+from math import sqrt
+
+
+from card import Card
 
 
 
+
+
+class Board:
+    def __init__(self, num_cards: int, padding: int) -> None:
+        """ Defines a board object, with the number of cards and padding between them """
+        self.total_cards = num_cards
+        self.card_row_length = self.rowify(num_cards)
+        self.card_rows = num_cards // self.card_row_length
+
+        #self.card_pos = self.generate_cards_positions(self.card_rows, self.card_row_length, padding)
+
+
+    def rowify(self, card_total: int) -> int:
+        """ Splits the number of cards into equal rows """
+        row_length = int(sqrt(card_total))
+        
+        for i in range(row_length, 0, -1):
+            if card_total % i == 0:
+                row_length = i
+                break
+
+        return row_length
+
+
+    def generate_cards_positions(self, rows: int, row_length: int, padding: int) -> list:
+        """ Places each cards at their coordinates """
+        game_field_width = min(CST.SCREEN_WIDTH, CST.SCREEN_HEIGHT)
+        card_size = (game_field_width - padding * (row_length + 1)) // row_length
+
+        screen_center = (CST.SCREEN_WIDTH//2, CST.SCREEN_HEIGHT//2)
+        board_size = card_size * row_length + padding * (row_length-1)
+        origin_point = (screen_center[0] - board_size // 2 + card_size // 2,
+                        screen_center[1] - board_size // 2 + card_size // 2)
+
+        coords_list = []
+        for row in range(row_length):
+            for col in range(row_length):
+                this_coord = (origin_point[0] + (card_size + padding)*col,
+                              origin_point[1] + (card_size + padding)*row)
+                coords_list.append(this_coord)
+        
+        
+        # Placing cards
+        self.card_list = []
+        for pos in coords_list:
+            self.card_list.append(Card(pos, card_size,CST.SHAPELIST[4], CST.CARDCOLORS[0], CST.CARD_BACK))
+
+        
+
+    def game_tick_update(self, window: pygame.Surface, mousepos: tuple, delta: float) -> None:
+        for card in self.card_list:
+            card.game_tick_update(window, mousepos, delta)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+# LOCAL TESTING
+if __name__ == "__main__":
+
+    import sys
+
+    pygame.init()
+
+    mainscreen = CST.MAINSCREEN
+    gameclock = pygame.time.Clock()
+    looping = True
+
+    dummyboard = Board(16, 5)
+    dummyboard.generate_cards_positions(4, 4,  30)
+    
+
+    while looping:
+        delta = gameclock.tick(CST.FPS)
+        mousepos = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                looping = False
+        
+
+        mainscreen.fill((125,125,125))
+        dummyboard.game_tick_update(mainscreen, mousepos, delta)
+        pygame.display.update()
+
+
+    pygame.quit()
+    sys.exit()
---
+++ src/mp_scene_level.py
@@ -0,0 +1,44 @@
+# MEMORY PUZZLE: Game level
+
+
+
+import pygame
+import constants as CST
+from master_class_scene import Scene
+from master_class_button import Button
+import mp_background as bg
+
+
+
+class Gamelevel(Scene):
+	def __init__(self, GAME_WINDOW: pygame.Surface) -> None:
+		super().__init__(GAME_WINDOW)
+
+    	# Scene Elements
+		background = bg.Background()
+		self.next_scene_params = {"next_scene": CST.SCENES.GAMEMENU,}
+
+    	# Append order is draw order
+		self.updatelist.append(background)
+    
+    
+	def load_outside_params(self, params: dict) -> None:
+        """ Loads a dictionary of parameters from another scene """
+        pass
+
+
+
+
+
+
+
+
+
+# TESTING
+if __name__ == "__main__":
+
+	test_menu = Gamelevel(CST.MAINSCREEN)
+
+	next_scene_params = {"next_scene": 0,}
+	next_scene_params = test_menu.run(next_scene_params)
+	print("Next scene:", next_scene_params["next_scene"])
