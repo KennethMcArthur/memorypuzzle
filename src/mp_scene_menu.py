0d065a77b94bd7bddceea7f2312e46c48615f077
Kenneth <canowa@libero.it>
2022-02-19 10:46:20 +0100

    layout adjustments

--- src/mp_scene_menu.py
+++ src/mp_scene_menu.py
@@ -16,36 +16,39 @@ class GameMenu(Scene):
 		super().__init__(GAME_WINDOW)
 
     	# Scene Elements
+		TITLE_SIZE = 80
 		HALF_SCREEN = CST.SCREEN_WIDTH//2
 		ONETHIRD_SCREEN = CST.SCREEN_WIDTH//3
-		FIRST_ROW = 100
-		SECOND_ROW = 300
-		THIRD_ROW = 400
+		FIRST_ROW = 50
+		SECOND_ROW = 350
+		THIRD_ROW = 450
 		FOURTH_ROW = 600
 		GRID = {
-			"title": (HALF_SCREEN, FIRST_ROW),
+			"title_up": (CST.SCREEN_WIDTH * 0.15, FIRST_ROW),
+			"title_down": (CST.SCREEN_WIDTH * 0.85, FIRST_ROW+TITLE_SIZE),
 			"b_size_label": (HALF_SCREEN, SECOND_ROW),
 			"minus_button": (ONETHIRD_SCREEN-25, THIRD_ROW-25, 50, 50),
 			"b_value": (HALF_SCREEN, THIRD_ROW),
 			"plus_button": (ONETHIRD_SCREEN*2-25, THIRD_ROW-25, 50, 50),
 			"start_button": (HALF_SCREEN-ONETHIRD_SCREEN//2, FOURTH_ROW, ONETHIRD_SCREEN, ONETHIRD_SCREEN//4),
-			"quit_button": (),
 		}
 
 		self.BOARD_SIZES = list(CST.BOARD_SIZE.keys())
 		self.board_size_index = 0
 		self.next_scene_params = {"next_scene": CST.SCENES.GAMEMENU,}
 		background = bg.Background()
-		big_title = StaticText("Memory Puzzle", 64, GRID["title"], alignment=StaticText.CENTER)
-		board_size_label = StaticText("Choose board size", 32, GRID["b_size_label"], alignment=StaticText.CENTER)
-		self.board_size_value_label = StaticText(str(self.BOARD_SIZES[self.board_size_index]), 64, GRID["b_value"], alignment=StaticText.CENTER)
+		big_title_up = StaticText("Memory", TITLE_SIZE, GRID["title_up"], alignment=StaticText.LEFT)
+		big_title_down = StaticText("Puzzle", TITLE_SIZE, GRID["title_down"], alignment=StaticText.RIGHT)
+		board_size_label = StaticText("Choose board size", TITLE_SIZE//2, GRID["b_size_label"], alignment=StaticText.CENTER)
+		self.board_size_value_label = StaticText(str(self.BOARD_SIZES[self.board_size_index]), TITLE_SIZE, GRID["b_value"], alignment=StaticText.CENTER)
 		start_button = Button("Start", CST.BUTTON_STYLE, GRID["start_button"], self.button_start)
 		plus_button = Button("+", CST.BUTTON_STYLE, GRID["plus_button"], self.button_plus)
 		minus_button = Button("-", CST.BUTTON_STYLE, GRID["minus_button"], self.button_minus)
 
     	# Append order is draw order
 		self.updatelist.append(background)
-		self.updatelist.append(big_title)
+		self.updatelist.append(big_title_up)
+		self.updatelist.append(big_title_down)
 		self.updatelist.append(plus_button)
 		self.updatelist.append(minus_button)
 		self.updatelist.append(board_size_label)
