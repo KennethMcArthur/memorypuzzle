41b406ada6d46e8445b575b16dfbf0637974396a
Kenneth <canowa@libero.it>
2022-03-01 09:42:43 +0100

    removed unused methods

--- src/master_class_scene.py
+++ src/master_class_scene.py
@@ -13,7 +13,6 @@ class Scene:
         self.GAME_WINDOW = GAME_WINDOW
         self.looping_active = True
         self.updatelist = []
-        self.game_timer_step = 1 # Every X seconds
 
 
     def event_checking(self, this_event: pygame.event) -> None:
@@ -21,16 +20,6 @@ class Scene:
         if this_event.type == pygame.QUIT: # Handling of quit event
             pygame.quit()
             sys.exit() # ensures we quit game AND program
-
-
-    def set_timer_step(self, seconds: int):
-        """ Allows to set a new seconds threshold for the internal game timer """
-        self.game_timer_step = int(seconds)
-
-
-    def timer_duty(self) -> None:
-        """ What needs to be done each time the timer goes off """
-        pass
 
 
     def quit_loop(self, data_to_return: dict) -> None:
@@ -50,11 +39,6 @@ class Scene:
         self.looping_active = True
 
 
-    def load_and_start_music(self) -> None:
-        """ To be overridden to manage a Scene starting music """
-        pass
-
-
     def text_to_update(self) -> None:
         """ To be overridden to update any text at the beginning of the scene """
         pass
@@ -68,19 +52,14 @@ class Scene:
     def run(self, outside_params: dict) -> int:
         """ Main loop method """
         self.load_outside_params(outside_params)
-        self.load_and_start_music() # Starting music only when Scene is active
         self.text_to_update()
         
         gameclock = pygame.time.Clock()
-        game_timer = 0
 
         # Main game loop
         while self.looping_active:
 
             delta = gameclock.tick(CST.FPS)
-
-            # What happens each frame
-            game_timer += 1
 
             for event in pygame.event.get():
                 self.event_checking(event)
@@ -91,11 +70,6 @@ class Scene:
             for gameobj in self.updatelist:
                 gameobj.game_tick_update(self.GAME_WINDOW, cursor_pos, delta) # All classes have this methods
             pygame.display.update()
-
-            # Game timer, used by scenes as they want                
-            if game_timer % (self.game_timer_step * CST.FPS) == 0: # every X seconds
-                game_timer = 0
-                self.timer_duty()
                 
         self.reset_state()
         return self.scene_return_data
