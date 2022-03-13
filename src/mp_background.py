6183c753e1aba30f65b0aeb7bb8146e33aaa6bb4
Kenneth <canowa@libero.it>
2022-03-13 10:18:13 +0100

    removed external constant

--- src/mp_background.py
+++ src/mp_background.py
@@ -2,10 +2,12 @@
 
 
 import pygame
-import constants as CST
+
 
 
 class Background:
 
+	BG_COLOR = pygame.Color("#2A9D8F")
+
 	def game_tick_update(self, window: pygame.Surface, *other_params) -> None:
-		window.fill(CST.COLOR.BACKGROUND)
+		window.fill(Background.BG_COLOR)
