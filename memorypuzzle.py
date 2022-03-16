6676005fada82611bec3fbb75f34e51664803832
Kenneth <canowa@libero.it>
2022-03-16 09:47:33 +0100

    removed redundant syntax

--- memorypuzzle.py
+++ memorypuzzle.py
@@ -12,31 +12,24 @@ from src import mp_scene_credits
 
 
 
-
-
 def main_game():
     pygame.init()
 
     GAMEWINDOW = pygame.display.set_mode((768, 768))
     pygame.display.set_caption("Memory Puzzle")
 
-
     # Scene initialization
-    gamemenu = mp_scene_menu.GameMenu(GAMEWINDOW)
-    gamelevel = mp_scene_level.Gamelevel(GAMEWINDOW)
-    gameendscreen = mp_scene_end.GameEnd(GAMEWINDOW)
-    gamecredits = mp_scene_credits.GameCredits(GAMEWINDOW)
-
     scenelist = [
-        gamemenu,
-        gamelevel,
-        gameendscreen,
-        gamecredits,
+        mp_scene_menu.GameMenu(GAMEWINDOW),
+        mp_scene_level.Gamelevel(GAMEWINDOW),
+        mp_scene_end.GameEnd(GAMEWINDOW),
+        mp_scene_credits.GameCredits(GAMEWINDOW)
     ]
 
     scene_params = {"next_scene": 0,}
-    while scene_params["next_scene"] != None:
-        scene_params = scenelist[scene_params["next_scene"]].run(scene_params)
+    while scene_params["next_scene"] is not None:
+        next_scene = scene_params["next_scene"]
+        scene_params = scenelist[next_scene].run(scene_params)
     
     pygame.quit()
     sys.exit()
